d7a26a5b8a3e5df3e55a12e349507758173f9b33
mehmetcanyegen <mehmetcan.yegen@gmail.com>
2017-08-21 02:05:52 +0300

    first

---
+++ checkdgb/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace checkdgb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.başlangıcaEkleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.başlangıçtanÇıkarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programıKapatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayarYenileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.başlangıcaEkleToolStripMenuItem,
+            this.başlangıçtanÇıkarToolStripMenuItem,
+            this.programıKapatToolStripMenuItem,
+            this.ayarYenileToolStripMenuItem,
+            this.hakkındaToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(172, 136);
+            // 
+            // başlangıcaEkleToolStripMenuItem
+            // 
+            this.başlangıcaEkleToolStripMenuItem.Name = "başlangıcaEkleToolStripMenuItem";
+            this.başlangıcaEkleToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.başlangıcaEkleToolStripMenuItem.Text = "Başlangıca Ekle";
+            this.başlangıcaEkleToolStripMenuItem.Click += new System.EventHandler(this.başlangıcaEkleToolStripMenuItem_Click);
+            // 
+            // başlangıçtanÇıkarToolStripMenuItem
+            // 
+            this.başlangıçtanÇıkarToolStripMenuItem.Name = "başlangıçtanÇıkarToolStripMenuItem";
+            this.başlangıçtanÇıkarToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.başlangıçtanÇıkarToolStripMenuItem.Text = "Başlangıçtan Çıkar";
+            this.başlangıçtanÇıkarToolStripMenuItem.Click += new System.EventHandler(this.başlangıçtanÇıkarToolStripMenuItem_Click);
+            // 
+            // programıKapatToolStripMenuItem
+            // 
+            this.programıKapatToolStripMenuItem.Name = "programıKapatToolStripMenuItem";
+            this.programıKapatToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.programıKapatToolStripMenuItem.Text = "Programı Kapat";
+            this.programıKapatToolStripMenuItem.Click += new System.EventHandler(this.programıKapatToolStripMenuItem_Click);
+            // 
+            // hakkındaToolStripMenuItem
+            // 
+            this.hakkındaToolStripMenuItem.Name = "hakkındaToolStripMenuItem";
+            this.hakkındaToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.hakkındaToolStripMenuItem.Text = "Hakkında";
+            this.hakkındaToolStripMenuItem.Click += new System.EventHandler(this.hakkındaToolStripMenuItem_Click);
+            // 
+            // ayarYenileToolStripMenuItem
+            // 
+            this.ayarYenileToolStripMenuItem.Name = "ayarYenileToolStripMenuItem";
+            this.ayarYenileToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.ayarYenileToolStripMenuItem.Text = "Ayar Yenile";
+            this.ayarYenileToolStripMenuItem.Click += new System.EventHandler(this.ayarYenileToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 157);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Check DGB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem başlangıcaEkleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem başlangıçtanÇıkarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programıKapatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayarYenileToolStripMenuItem;
+
+    }
+}
+
---
+++ checkdgb/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.Win32;
+using System.IO;
+
+
+namespace checkdgb
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Activated += Form1_Activated;
+
+        }
+
+        void Form1_Activated(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void AyarCek()
+        {
+            try
+            {
+                var tmp = File.ReadAllText("Ayar.txt").Trim();
+                var aralik = tmp.Split('=')[1].ToString().Trim();
+                var aralikInt = Int32.Parse(aralik);
+                timer1.Interval = aralikInt;
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refreshCoin();
+            AyarCek();
+            timer1.Start();
+
+         
+        }
+
+        private void SetStartup()
+        {
+            try
+            {
+                 RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            //if (chkStartUp.Checked)
+            rk.SetValue("checkdgb", Application.ExecutablePath.ToString());
+            //else
+            //    rk.DeleteValue("checkdgb",false);            
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+        }
+
+        private void DeleteStartup()
+        {
+            try
+            {
+                RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+                rk.DeleteValue("checkdgb", false);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        private void refreshCoin()
+        {
+            var tmp = "";
+            try
+            {
+                tmp = checkCoin();
+            }
+            catch (Exception)
+            {
+                tmp = "!";
+            }
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+            try
+            {
+                tmp = editNumbers(tmp);
+            }
+            catch (Exception ex)
+            {
+
+                tmp = "!";
+            }
+            //   Console.WriteLine(r);
+            CreateTextIcon(tmp);
+        }
+
+        public void CreateTextIcon(string str)
+        {
+            Font fontToUse = new Font("Microsoft Sans Serif", 9, FontStyle.Regular, GraphicsUnit.Pixel);
+            Brush brushToUse = new SolidBrush(Color.White);
+            Bitmap bitmapText = new Bitmap(16, 16);
+            Graphics g = System.Drawing.Graphics.FromImage(bitmapText);
+
+            IntPtr hIcon;
+
+            g.Clear(Color.Transparent);
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+            g.DrawString(str, fontToUse, brushToUse, -2, -2);
+            hIcon = (bitmapText.GetHicon());
+            notifyIcon1.Icon = System.Drawing.Icon.FromHandle(hIcon);
+            //DestroyIcon(hIcon.ToInt32);
+        }
+
+        private string checkCoin()
+        {
+            var url = new Uri("https://api.coinmarketcap.com/v1/ticker/digibyte/?convert=BTC");
+            var tmpr = "";
+            try
+            {
+                using (var webClient = new System.Net.WebClient())
+                {
+                    var str = webClient.DownloadString(url);
+                    Newtonsoft.Json.Linq.JArray jar = (Newtonsoft.Json.Linq.JArray)JsonConvert.DeserializeObject(str);
+
+                    foreach (JObject item in jar)
+                    {
+                        JToken prc = item.GetValue("price_btc");
+                        tmpr = prc.ToString();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                tmpr = "!";
+            }
+            return tmpr;
+        }
+
+        private string editNumbers(string nmb)
+        {
+            nmb = nmb.Split('.')[1];
+            var tmp = "";
+            var tmp2 = "";
+            var count = 0;
+            for (int i = 0; i < nmb.Length; i++)
+            {
+                if (nmb[i].ToString() != "0")
+                    count++;
+                if (count > 0)
+                {
+                    tmp += nmb[i].ToString();
+                }
+
+            }
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                if (i < 3)
+                {
+                    tmp2 += tmp[i];
+                }
+            }
+            return tmp2;
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            refreshCoin();
+            //Console.WriteLine(DateTime.Now.ToLongTimeString());
+
+        }
+
+        private void programıKapatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void başlangıcaEkleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetStartup();
+
+        }
+
+        private void başlangıçtanÇıkarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DeleteStartup();
+        }
+
+        private void hakkındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("iletisim@mehmetcanyegen.com.tr", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ayarYenileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AyarCek();
+        }
+
+    }
+
+}
