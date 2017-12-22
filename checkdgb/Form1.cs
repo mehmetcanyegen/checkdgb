858a47ce425627c59776daabe6906d8abd2a6f3d
mehmetcanyegen <mehmetcan.yegen@gmail.com>
2017-12-22 05:43:02 +0300

    cleaned some comment lines

--- checkdgb/Form1.cs
+++ checkdgb/Form1.cs
@@ -147,10 +147,8 @@ namespace checkdgb
         {
             var url = new Uri("https://api.coinmarketcap.com/v1/ticker/digibyte/?convert=BTC");
             DateTime d1 = DateTime.Now;
-            //var url = new Uri("https://bittrex.com/api/v1.1/public/getmarketsummaries");
             DateTime d2 = DateTime.Now;
 
-          //  MessageBox.Show(d1.ToLongTimeString() + " \n " + d2.ToLongTimeString());
             var tmpr = "";
             try
             {
